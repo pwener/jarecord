d1c9dc63c278d66397fbcaa6787720240eed4f22
pwener <phwener@gmail.com>
2017-06-14 12:43:50 -0300

    Cast to ArrayList

--- src/main/java/io/github/pwener/jarecord/activerecord/ActiveRecord.java
+++ src/main/java/io/github/pwener/jarecord/activerecord/ActiveRecord.java
@@ -124,7 +124,9 @@ public abstract class ActiveRecord<ActiveType> implements Serializable {
 		ActiveRecord<Entity> result = null;
 
 		List<ActiveRecord<Entity>> allResults = new ArrayList<>();
-		allResults.addAll((Collection<? extends ActiveRecord<Entity>>) FinderSingleton.get().get(attr, value));
+		allResults.addAll(
+				(ArrayList<? extends ActiveRecord<Entity>>) FinderSingleton.get()
+					.get(attr, value));
 
 		if (!allResults.isEmpty()) {
 			result = allResults.get(FIRST);
